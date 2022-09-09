a318c4305ccfa8431ec2b96a129391a2a8593458
tak <wontaek22@gmail.com>
2022-09-09 23:41:06 +0900

    22.09.09
    Study

---
+++ src/com/tak/study/Study.java
@@ -0,0 +1,15 @@
+package com.tak.study;
+
+public class Study {
+
+    public static void main(String[] args) {
+
+        System.out.println("요즘 교육기간이라 교육자료를 통해 공부를 하고있다" +
+                "교육 자료를 commit하기에는 조금 그래서.." +
+                "일단은 교육 전에는 ajax에 대해 무지한 정도였는데" +
+                "이제는 맨 땅에 헤딩으로 ajax를 다 사용하며" +
+                "어느정도 이해를 하고 있다." +
+                "조만간 blog에 내가 이해한 ajax에 대해 게시글을 게시할 예정이다." +
+                "글을 보며 추후에도 remind 해야겠다!");
+    }
+}
